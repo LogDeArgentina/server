58a44a080356c6bd42ab272d724cf307c4915214
Joaquín <63993644+RealDrPuc@users.noreply.github.com>
2023-12-24 13:53:23 -0300

    first commit

---
+++ src/main/java/me/drpuc/lda/LdaApplication.java
@@ -0,0 +1,11 @@
+package me.drpuc.lda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LdaApplication.class, args);
+	}
+}
---
+++ src/test/java/me/drpuc/lda/LdaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.drpuc.lda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
