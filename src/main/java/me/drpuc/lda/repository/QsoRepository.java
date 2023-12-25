cdb042c170e92a1586a641e03808ebf9b196259c
Joaquín <63993644+RealDrPuc@users.noreply.github.com>
2023-12-24 21:23:55 -0300

    add QsoRepository

---
+++ src/main/java/me/drpuc/lda/repository/QsoRepository.java
@@ -0,0 +1,8 @@
+package me.drpuc.lda.repository;
+
+import me.drpuc.lda.entity.Qso;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QsoRepository extends JpaRepository<Qso, String> {
+
+}
