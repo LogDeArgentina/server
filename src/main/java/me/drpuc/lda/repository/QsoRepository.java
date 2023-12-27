72fd9ee664ff4c7c4348ddafa7d6e3bd4d38dacd
Joaquín <63993644+RealDrPuc@users.noreply.github.com>
2023-12-27 13:00:59 -0300

    fix import style

--- src/main/java/me/drpuc/lda/repository/QsoRepository.java
+++ src/main/java/me/drpuc/lda/repository/QsoRepository.java
@@ -2,6 +2,7 @@ package me.drpuc.lda.repository;
 
 import me.drpuc.lda.entity.Qso;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
