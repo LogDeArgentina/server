3874c764065ac081e87f0149008ccd0be48237b4
Joaquín <63993644+RealDrPuc@users.noreply.github.com>
2023-12-28 16:13:37 -0300

    feat: get unverified users

--- src/main/java/me/drpuc/lda/controller/VerificationController.java
+++ src/main/java/me/drpuc/lda/controller/VerificationController.java
@@ -8,12 +8,24 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/verification")
 public class VerificationController {
     private final UserService userService;
     private final UserRepository userRepository;
+
+    @GetMapping("/due")
+    public List<User> getDueUsers(Authentication auth) {
+        User requestingUser = userService.getUserViaAuthentication(auth);
+        if (!requestingUser.getRole().equals("ADMIN")) {
+            throw new AccessDeniedException("illegal access");
+        }
+
+        return userRepository.findByVerified(false);
+    }
 
     @PostMapping("/{userUuid}")
     public void verify(Authentication auth,
--- src/main/java/me/drpuc/lda/repository/UserRepository.java
+++ src/main/java/me/drpuc/lda/repository/UserRepository.java
@@ -3,10 +3,12 @@ package me.drpuc.lda.repository;
 import me.drpuc.lda.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByEmail(String email);
     Optional<User> findByUuid(String uuid);
+    List<User> findByVerified(boolean verified);
     boolean existsByCallsignOrEmail(String callsign, String email);
 }
