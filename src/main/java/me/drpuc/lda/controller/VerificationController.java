ee812a8503423c284d5b552829837dceefdde3fa
Joaquín <63993644+RealDrPuc@users.noreply.github.com>
2023-12-28 16:37:15 -0300

    protect Verification with SecurityConfig

--- src/main/java/me/drpuc/lda/controller/VerificationController.java
+++ src/main/java/me/drpuc/lda/controller/VerificationController.java
@@ -3,9 +3,6 @@ package me.drpuc.lda.controller;
 import lombok.RequiredArgsConstructor;
 import me.drpuc.lda.entity.User;
 import me.drpuc.lda.service.VerificationService;
-import me.drpuc.lda.service.UserService;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,38 +11,20 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/api/verification")
 public class VerificationController {
-    private final UserService userService;
     private final VerificationService verificationService;
 
     @GetMapping("/due")
-    public List<User> getDueUsers(Authentication auth) {
-        User requestingUser = userService.getUserViaAuthentication(auth);
-        if (!requestingUser.getRole().equals("ADMIN")) {
-            throw new AccessDeniedException("illegal access");
-        }
-
+    public List<User> getDueUsers() {
         return verificationService.getUnverifiedUsers();
     }
 
     @PostMapping("/{userUuid}")
-    public void verify(Authentication auth,
-                         @PathVariable String userUuid) {
-        User requestingUser = userService.getUserViaAuthentication(auth);
-        if (!requestingUser.getRole().equals("ADMIN")) {
-            throw new AccessDeniedException("illegal access");
-        }
-
+    public void verify(@PathVariable String userUuid) {
         verificationService.verify(userUuid);
     }
 
     @DeleteMapping("/{userUuid}")
-    public void unverify(Authentication auth,
-                         @PathVariable String userUuid) {
-        User requestingUser = userService.getUserViaAuthentication(auth);
-        if (!requestingUser.getRole().equals("ADMIN")) {
-            throw new AccessDeniedException("illegal access");
-        }
-
+    public void unverify(@PathVariable String userUuid) {
         verificationService.unverify(userUuid);
     }
 }
