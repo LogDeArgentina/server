6864aa5b07d8264c97220d029051d9163b6c43c9
Joaquín <63993644+RealDrPuc@users.noreply.github.com>
2023-12-30 01:07:38 -0300

    feat: health check

---
+++ src/main/java/me/drpuc/lda/controller/HealthController.java
@@ -0,0 +1,12 @@
+package me.drpuc.lda.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/health")
+public class HealthController {
+    @GetMapping
+    public void health() {}
+}
